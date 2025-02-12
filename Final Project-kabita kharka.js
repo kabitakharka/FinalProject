7d1d42f567b49c96e3e1c05e9e6ce36e1db87daa
Kabita Kharka <kabita.knarka2019@gmail.com>
2025-02-12 13:37:28 +1100

    initial commit

---
+++ Final Project-kabita kharka.js
@@ -0,0 +1,20 @@
+const slidesContainer = document.getElementById("slider");
+  const slides = document.querySelectorAll(".slider-item");
+  const prevButton = document.getElementById("prevbtn");
+  const nextButton = document.getElementById("nextbtn");
+  
+  console.log(slidesContainer.clientWidth);
+
+  nextButton.addEventListener("click", function() {
+    const slideWidth = slides[0].clientWidth; // Width of one slide
+    const visibleSlidesCount = Math.floor(slidesContainer.clientWidth / slideWidth); // Number of slides visible at once
+    const totalWidthToScroll = slideWidth * visibleSlidesCount; // Total width to scroll for multiple visible slides
+    slidesContainer.scrollLeft += totalWidthToScroll;
+  });
+
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slides[0].clientWidth; // Width of one slide
+    const visibleSlidesCount = Math.floor(slidesContainer.clientWidth / slideWidth); // Number of slides visible at once
+    const totalWidthToScroll = slideWidth * visibleSlidesCount; // Total width to scroll for multiple visible slides
+    slidesContainer.scrollLeft -= totalWidthToScroll;
+  });
